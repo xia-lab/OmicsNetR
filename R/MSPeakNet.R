9a49f17889e5023d697f4bad4d0afec937b64ce7
Zhiqiang Pang <zhiqiang.pang@mail.mcgill.ca>
2024-07-22 15:43:54 -0400

    LC-MS input - bug fixes - 2

--- R/MSPeakNet.R
+++ R/MSPeakNet.R
@@ -178,7 +178,7 @@ PerformDataProcessing <- function() {
   PeakSet$StructureSet <- StructureSet;
   PeakSet$EdgeSet_all <- EdgeSet_all;
   PeakSet <<- PeakSet;
-  message("Peak data processing completed!");
+  print("Peak data processing completed!");
   return(1L)
 }
 
@@ -208,7 +208,7 @@ PerformPropagation <- function() {
   PeakSet$StructureSet_df <- StructureSet_df;
   cat("done!\n Time consumed this step: ", Sys.time() - t0, "\n")
   PeakSet <<- PeakSet;
-  message("Peak data constructing propagation completed!");
+  print("Peak data constructing propagation completed!");
   return(1L);
 }
 
@@ -220,15 +220,15 @@ PerformGlobalOptimization <- function() {
   if(exists("PeakSet",envir = .GlobalEnv)) {
     PeakSet <- get("PeakSet", envir = .GlobalEnv);
   } else {
-    return(0);
+    return(0L);
   }
   cat("Start Preparing ILP Set for optimization...\n")
   PeakSet$ILPSet <- .prepareILPSet(PeakSet);
   PeakSet <- .networkConstruct(PeakSet);
   cat("Global done!\n Time consumed this step: ", Sys.time() - t0, "\n")
   PeakSet <<- PeakSet;
-  message("Global optimization completed!");
-  return(1);
+  print("Global optimization completed!");
+  return(1L);
 }
 
 #' PerformAnnotation
@@ -239,7 +239,7 @@ PerformAnnotation <- function() {
     if(exists("PeakSet",envir = .GlobalEnv)) {
         PeakSet <- get("PeakSet", envir = .GlobalEnv)
     } else {
-        return(0)
+        return(0L)
     }
     cat("Start Perform Network Annotation...\n")
     ResSet <- path_annotation(PeakSet$ILPSet,
@@ -280,8 +280,8 @@ PerformAnnotation <- function() {
     PeakSet <<- PeakSet <- .cleanPeakSet(PeakSet);
     qs::qsave(PeakSet, file = "PeakSet_done.qs");
     cat("Annotation done!\n Time consumed this step: ", Sys.time() - t0, "\n")
-    message("Peak annotation completed!");
-    return(1)
+    print("Peak annotation completed!");
+    return(1L)
 }
 
 GetFastPeak <- function(){
