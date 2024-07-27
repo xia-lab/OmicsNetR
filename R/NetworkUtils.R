5e1ab7c23c0f574ce921dac1c6542a901c644ea6
jeffxia <jeffxia@192.168.2.33>
2024-07-27 10:30:51 -0400

    Move infrequent functions to lazy loading mode

--- R/NetworkUtils.R
+++ R/NetworkUtils.R
@@ -1628,89 +1628,12 @@ PlotBetweennessHistogram <- function(imgNm, netNm = "NA",dpi=72, format="png"){
 #' @export
 
 PreparePeaksNetwork <- function(dataSetObj=NA){
-  dataSet <- .get.nSet(dataSetObj);
-  if(exists("PeakSetDone",envir = .GlobalEnv)) {
-    PeakSet <- get("PeakSetDone", envir = .GlobalEnv);
-  } else if(exists("PeakSet",envir = .GlobalEnv)) {
-    PeakSet <- get("PeakSet", envir = .GlobalEnv);
-  } else {
-    return(0L);
-  }
-  nodes <- PeakSet$nodes;
-  edges <- PeakSet$edges;
-  met.kg.ids <- nodes$KEGGID;
-
-  tmpsIDs <- nodes$HMDBID;
-  tmpsIDs <- tmpsIDs[tmpsIDs != ""]
-  if(all(grepl("^C[0-9]+", tmpsIDs))){
-    met.ids <- nodes$KEGGID;
-    met.ids[met.ids == ""] <- nodes$formula[met.ids == ""];
-    lbls <- doKegg2NameMapping(met.ids);
-  } else if (all(grepl("^HMDB[0-9]+", tmpsIDs))) {
-    met.ids <- nodes$HMDBID;
-    met.ids[met.ids == ""] <- nodes$formula[met.ids == ""];
-    lbls <- doHMDB2NameMapping(met.ids);
-  } else if (all(grepl("^PBCM[0-9]+", tmpsIDs))) {
-    met.ids <- nodes$HMDBID;
-    met.ids[met.ids == ""] <- nodes$formula[met.ids == ""];
-    lbls <- doPubchem2NameMapping(met.ids);
-  }
-
-  met.kg.ids[met.kg.ids == ""] <- nodes$formula[met.kg.ids == ""];
-  PeakSet$nodes$KEGGID_realID <- met.kg.ids;
-  PeakSet$nodes$KEGGID <- met.ids;
-  art.ids <- PeakSet$nodes$name[which(PeakSet$nodes$class == "Artifact")]
-
-  PeakSet$mets <- unique(PeakSet$nodes$KEGGID[which(PeakSet$nodes$class == "Metabolite")]);
-  PeakSet$put.mets <- PeakSet$nodes$KEGGID[which(PeakSet$nodes$class == "Putative metabolite")];
-  PeakSet$mets.ids <- PeakSet$nodes$name[which(PeakSet$nodes$class == "Metabolite")];
-  PeakSet$put.mets.ids <- PeakSet$nodes$name[which(PeakSet$nodes$class == "Putative metabolite")];
-
-
-  PeakSet$nodes.df <- data.frame(Id=met.ids, Label=lbls)
-  edges.df0 <- data.frame(from=edges[,1], to=edges[,2])
-  edges.df01 <- edges.df0[!edges.df0[,1] %in% art.ids,];
-  edges.df02 <- edges.df01[!edges.df01[,2] %in% art.ids,];
-  conv1 <- data.frame(from=PeakSet$nodes$name, Source=PeakSet$nodes$KEGGID);
-  conv2 <- data.frame(to=PeakSet$nodes$name, Target=PeakSet$nodes$KEGGID);
-  edges.df1 <- merge(edges.df02, conv1, by = "from");
-  edges.df2 <- merge(edges.df1, conv2, by = "to");
-  PeakSet$edges.df <- data.frame(Source=edges.df2$Source, Target=edges.df2$Target);
-
-    if(length(which(grepl("HMDB", PeakSet$mets)))/length(PeakSet$mets) > 0){
-      PeakSet$mets <- doHMDB2KEGGMapping(PeakSet$mets);
-      PeakSet$nodes.df[,1] <- doHMDB2KEGGMapping(PeakSet$nodes.df[,1]);
-      PeakSet$edges.df[,1] <- doHMDB2KEGGMapping(PeakSet$edges.df[,1]);
-      PeakSet$edges.df[,2] <- doHMDB2KEGGMapping(PeakSet$edges.df[,2]);
-
+    if(!exists("my.peak.net")){ # public web on same user dir
+        compiler::loadcmp("../../rscripts/OmicsNetR/R/utils_peak_net.Rc");
     }
-    if(length(which(grepl("PBCM", PeakSet$mets)))/length(PeakSet$mets) > 0){
-      PeakSet$mets <- doPubchem2KEGGMapping(PeakSet$mets);
-      PeakSet$nodes.df[,1] <- doPubchem2KEGGMapping(PeakSet$nodes.df[,1]);
-      PeakSet$edges.df[,1] <- doPubchem2KEGGMapping(PeakSet$edges.df[,1]);
-      PeakSet$edges.df[,2] <- doPubchem2KEGGMapping(PeakSet$edges.df[,2]);
-
-    }
-
-  dataSet$seed[["peak"]] <- data.frame(rep(0, length(unique(PeakSet$mets))));
-
-  rownames(dataSet$seed[["peak"]]) <-unique(PeakSet$mets);
-  dataSet$exp.mat[["peak"]] <- data.frame(rep(0, length(PeakSet$NetID_output$peak_id)));
-  rownames(dataSet$exp.mat[["peak"]]) <- paste0(PeakSet$NetID_output$peak_id,"_", PeakSet$NetID_output$medMz);
-
-  cmp.nms <- PrepareInputList(dataSet, rownames(dataSet$seed[["peak"]]), data.org, "peak", "kegg");
-
-  qs:::qsave(PeakSet, "PeakSet_net.qs");
-  dataSet <<- dataSet;
-  if(.on.public.web){
-    .set.nSet(dataSet);
-    return(1L);
-  }else{
-    return(.set.nSet(dataSet));
-  }
+    res <- my.peak.net(dataSetObj);
+    return(res);
 }
-
-
 
 
 #' Guilt-by-association analysis using Random Walk With Restart
---
+++ R/utils_reg_enrich.R
@@ -0,0 +1,118 @@
+  
+my.reg.enrich <- function(file.nm, fun.type, ora.vec, netInv, idType){
+  require("plyr")
+  ora.nms <- names(ora.vec);
+  # prepare for the result table
+  set.size<-100;
+
+  if (fun.type %in% c("chea", "encode", "jaspar", "trrust")){
+    table.nm <- paste(data.org, fun.type, sep="_");
+    res <- QueryTFSQLite(table.nm, ora.vec, netInv);
+    # no hits
+    if(nrow(res)==0){ return(c(0,0)); }
+    edge.res <- data.frame(gene=res[,"entrez"], symbol=res[,"symbol"],id=res[,"tfid"], name=res[,"tfname"]);
+    node.ids <- c(res[,"entrez"], res[,"tfid"]);
+    node.nms <- c(res[,"symbol"], res[,"tfname"]);
+
+  }else if(fun.type == "mirnet"){ # in miRNA, table name is org code, colname is id type
+    table.nm <- data.org
+    res <- QueryMirSQLite(table.nm, "entrez", ora.vec, "inverse", data.org);
+    if(nrow(res)==0){ return(c(0,0)); }
+    edge.res <- data.frame(gene=res[,"entrez"], symbol=res[,"symbol"], id=res[,"mir_acc"], name=res[,"mir_id"] );
+    node.ids <- c(res[,"entrez"], res[,"mir_acc"])
+    node.nms <- c(res[,"symbol"], res[,"mir_id"]);
+
+  }else if(fun.type == "met"){
+    table.nm <- paste(data.org, "kegg", sep="_");
+    res <- QueryMetSQLiteNet(table.nm, ora.vec, "inverse");
+    if(nrow(res)==0){ return(c(0,0)); }
+    edge.res <- data.frame(gene=res[,"entrez"], symbol=res[,"symbol"], id=res[,"kegg"], name=res[,"met"] );
+    node.ids <- c(res[,"entrez"], res[,"kegg"])
+    node.nms <- c(res[,"symbol"], res[,"met"]);
+
+  }else{
+    table.nm <- paste("drug", data.org, sep="_");
+    ora.vec <- doEntrez2UniprotMapping(ora.vec);
+    res <- QueryDrugSQLite(ora.vec);
+    if(nrow(res)==0){ return(c(0,0)); }
+    edge.res <- data.frame(gene=doUniprot2EntrezMapping(res[,"upid"]), symbol=res[,"symbol"], id=res[,"dbid"], name=res[,"dbname"] );
+    node.ids <- c(doUniprot2EntrezMapping(res[,"upid"]), res[,"dbid"])
+    node.nms <- c(res[,"symbol"], res[,"dbname"]);
+  }
+
+  edge.res$mix <- paste0(edge.res[,1], edge.res[,3]);
+  edge.res <- edge.res[!duplicated(edge.res$mix),];
+
+  row.names(edge.res) <- 1:nrow(edge.res);
+  fast.write.csv(edge.res, file="orig_edge_list.csv",row.names=FALSE);
+
+  hit.freq <- count(edge.res, 'id')
+  hit.freq <- hit.freq[order(hit.freq$freq, decreasing = TRUE),]
+  hit.freqnm <- count(edge.res, 'name')
+  hit.freqnm <- hit.freqnm[order(hit.freqnm$freq, decreasing = TRUE),]
+  hit.freq$name <- hit.freqnm$name
+  hits.gene <- list()
+
+  if(idType == "uniprot"){
+    for(i in 1:nrow(hit.freq)){
+      df <- edge.res[which(edge.res$id == hit.freq$id[i]),];
+      gene.vec <- as.vector(df$gene);
+      gene.vec <- doEntrez2UniprotMapping(gene.vec)
+      hits.gene[[i]] <- gene.vec;
+    }
+  } else if(idType == "entrez") {
+    for(i in 1:nrow(hit.freq)){
+      df <- edge.res[which(edge.res$id == hit.freq$id[i]),];
+      gene.vec <- as.vector(df$gene);
+      hits.gene[[i]] <- gene.vec;
+    }
+  }else {
+    for(i in 1:nrow(hit.freq)){
+      df <- edge.res[which(edge.res$id == hit.freq$id[i]),];
+      gene.vec <- as.vector(df$gene);
+      gene.vec <- doEntrez2EmblProteinMapping(gene.vec)
+      hits.gene[[i]] <- gene.vec;
+    }
+  }
+  names(hits.gene) = hit.freq$name;
+
+  resTable1 = data.frame(hit.freq$id, hit.freq$name, hit.freq$freq)
+  colnames(resTable1) = c("Id", "Name", "Hits")
+  resTable1 = resTable1[order(-resTable1$Hits),]
+  current.msg <<- "Regulatory element enrichment analysis was completed";
+
+  if(regBool == "true"){
+    resTable1 <- resTable1[which(resTable1$Hits == regCount),]
+  }
+  # write json
+  require("RJSONIO");
+  fun.ids <- resTable1[,1];
+  fun.nms <- resTable1[,2];
+  fun.hits <- resTable1[,3];
+  json1.res <- list(
+    fun.ids = fun.ids,
+    fun.nms = fun.nms,
+    fun.hits = fun.hits,
+    fun.genes = hits.gene
+  );
+  json.mat <- toJSON(json1.res);
+  json.nm <- paste(file.nm, ".json", sep="");
+  sink(json.nm)
+  cat(json.mat);
+  sink();
+
+  resTable1$Features = hits.gene
+  # write csv
+  csv.nm <- paste(file.nm, ".csv", sep="");
+  fast.write.csv(resTable1, file=csv.nm, row.names=F);
+
+  #record table for report
+  type = "regNetwork";
+  dataSet$imgSet$enrTables[[type]] <- list()
+  dataSet$imgSet$enrTables[[type]]$table <- resTable1;
+  dataSet$imgSet$enrTables[[type]]$library <- fun.type
+  dataSet$imgSet$enrTables[[type]]$algo <- "Overrepresentation Analysis"
+  dataSet <<- dataSet
+
+  return(1);
+}
